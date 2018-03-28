95578ff6a2483aa92ed51f8f5920788c4188428b
Frederick Gnodtke <frederick@gnodtke.net>
2018-03-28 08:06:03 +0200

    Refactor from getters to copies when creating PngImage instances

--- src/png-image.ts
+++ src/png-image.ts
@@ -65,85 +65,185 @@ export enum InterlaceType {
     UNKNOWN = "unknown",
 }
 
-export class PngImage {
-    private nativePng: any;
+/**
+ * Converts the native time from the libpng bindings into a javascript `Date` object.
+ *
+ * @param nativeTime The time as returned by the bindings.
+ *
+ * @return The time converted to a javascript `Date` object or `undefined` if the time was
+ *         not set in the PNG's header.
+ */
+function convertNativeTime(nativeTime: any): Date {
+    if (!nativeTime) { return; }
+    const { year, month, day, hour, minute, second } = nativeTime;
+    return new Date(year, month, day, hour, minute, second);
+}
 
+/**
+ * Converts the background color from the libpng bindings into a color.
+ *
+ * @param nativeBackgroundColor The background color as returned by the native bindings.
+ * @param colorType The color type of the image of which the background color should be converted.
+ *
+ * @return The converted background color in `ColorRGB`, `ColorPalette` or `ColorGrayScale` format
+ *         or undefined if the background color was not set in the PNG.
+ */
+function convertNativeBackgroundColor(
+    nativeBackgroundColor: any,
+    colorType: ColorType,
+): ColorRGB | ColorGrayScale | ColorPalette {
+    if (!nativeBackgroundColor) { return; }
+    switch (colorType) {
+        case ColorType.GRAY_SCALE:
+        case ColorType.GRAY_SCALE_ALPHA:
+            return colorGrayScale(nativeBackgroundColor.gray);
+        case ColorType.PALETTE:
+            return colorPalette(nativeBackgroundColor.index);
+        case ColorType.RGB:
+        case ColorType.RGBA:
+            const { red, green, blue } = nativeBackgroundColor;
+            return colorRGB(red, green, blue);
+        default:
+            return undefined;
+    }
+}
+
+/**
+ * Converts a native palette as returned by the bindings into a Map.
+ *
+ * @param nativePalette The native palette which should be converted.
+ *
+ * @return The palette as a `Palette` (Javascript Map with the key being the palette index and
+ *         the value being a color.
+ */
+function convertNativePalette(nativePalette: any): Palette {
+    if (!nativePalette) { return; }
+    return nativePalette.reduce((result: Palette, current: any, index: number) => {
+        result.set(index, colorRGB(current.red, current.green, current.blue));
+        return result;
+    }, new Map<number, ColorRGB>());
+}
+
+/**
+ * Decodes and wraps a PNG image. Will call the native bindings under the hood and provides
+ * a high-level access to read- and write operations on the image.
+ */
+export class PngImage {
     constructor(buffer: Buffer) {
         if (!Buffer.isBuffer(buffer)) {
             throw new Error("Error decoding PNG. Input is not a buffer.");
         }
-        this.nativePng = new __native_PngImage(buffer);
+        const nativePng = new __native_PngImage(buffer);
+        this.bitDepth = nativePng.bitDepth;
+        this.channels = nativePng.channels;
+        this.colorType = nativePng.colorType;
+        this.height = nativePng.height;
+        this.width = nativePng.width;
+        this.interlaceType = nativePng.interlaceType;
+        this.rowBytes = nativePng.rowBytes;
+        this.offsetX = nativePng.offsetX;
+        this.offsetY = nativePng.offsetY;
+        this.pixelsPerMeterX = nativePng.pixelsPerMeterX;
+        this.pixelsPerMeterY = nativePng.pixelsPerMeterY;
+        this.data = nativePng.data;
+        this.palette = convertNativePalette(nativePng.palette);
+        this.gamma = nativePng.gamma;
+        this.time = convertNativeTime(nativePng.time);
+        this.backgroundColor = convertNativeBackgroundColor(nativePng.backgroundColor, this.colorType);
     }
 
     /**
      * The bit depth of the image.
      * Gathered from `png_get_bit_depth`.
      */
-    public get bitDepth(): number { return this.nativePng.bitDepth; }
+    public readonly bitDepth: number;
 
     /**
      * The amount of channels of the image.
      * Gathered from `png_get_channels`.
      */
-    public get channels(): number { return this.nativePng.channels; }
+    public readonly channels: number;
 
     /**
      * The color type of the image as a string.
      * Gathered from `png_get_color_type`.
      */
-    public get colorType(): ColorType { return this.nativePng.colorType; }
+    public readonly colorType: ColorType;
 
     /**
      * The width of the image.
      * Gathered from `png_get_image_height`.
      */
-    public get height(): number { return this.nativePng.height; }
+    public readonly height: number;
 
     /**
      * The width of the image.
      * Gathered from `png_get_image_width`.
      */
-    public get width(): number { return this.nativePng.width; }
+    public readonly width: number;
 
     /**
      * The interlace type of the image as a string, gathered from `png_get_interlace_type`.
      */
-    public get interlaceType(): InterlaceType { return this.nativePng.interlaceType; }
+    public readonly interlaceType: InterlaceType;
 
     /**
      * The amount of bytes per row of the image.
      * Gathered from `png_get_rowbytes`.
      */
-    public get rowBytes(): number { return this.nativePng.rowBytes; }
+    public readonly rowBytes: number;
 
     /**
      * The horizontal offset of the image.
      * Gathered from `png_get_x_offset_pixels`.
      */
-    public get offsetX(): number { return this.nativePng.offsetX; }
+    public readonly offsetX: number;
 
     /**
      * The vertical offset of the image.
      * Gathered from `png_get_y_offset_pixels`.
      */
-    public get offsetY(): number { return this.nativePng.offsetY; }
+    public readonly offsetY: number;
 
     /**
      * The horizontal amount of pixels per meter of the image.
      * Gathered from `png_get_x_pixels_per_meter`.
      */
-    public get pixelsPerMeterX(): number { return this.nativePng.pixelsPerMeterX; }
+    public readonly pixelsPerMeterX: number;
 
     /**
      * The vertical amount of pixels per meter of the image.
      * Gathered from `png_get_y_pixels_per_meter`.
      */
-    public get pixelsPerMeterY(): number { return this.nativePng.pixelsPerMeterY; }
+    public readonly pixelsPerMeterY: number;
 
     /**
      * The buffer containing the data of the decoded image.
      */
-    public get data(): Buffer { return this.nativePng.data; }
+    public readonly data: Buffer;
+
+    /**
+     * Returns the last modification time as returned by `png_get_tIME`.
+     */
+    public readonly time: Date;
+
+    /**
+     * Returns the background color of the image if provided in the header.
+     */
+    public readonly backgroundColor: ColorRGB | ColorGrayScale | ColorPalette;
+
+    /**
+     * Retrieve the palette of this image if the color type is `ColorType.PALETTE`.
+     *
+     * @see ColorType
+     */
+    public readonly palette: Palette;
+
+    /**
+     * The gamma value of the image.
+     * Gathered from `png_get_gAMA`.
+     */
+    public readonly gamma: number;
 
     /**
      * Will be `true` if the image's color type has an alpha channel and `false` otherwise.
@@ -172,36 +272,6 @@ export class PngImage {
                 return 1;
             case ColorType.RGB:
                 return 3;
-            default:
-                return undefined;
-        }
-    }
-
-    /**
-     * Returns the last modification time as returned by `png_get_tIME`.
-     */
-    public get time(): Date {
-        const { time } = this.nativePng;
-        if (!time) { return; }
-        const { year, month, day, hour, minute, second } = time;
-        return new Date(year, month, day, hour, minute, second);
-    }
-
-    /**
-     * Returns the background color of the image if provided in the header.
-     */
-    public get backgroundColor(): ColorRGB | ColorGrayScale | ColorPalette {
-        const { backgroundColor } = this.nativePng;
-        if (!backgroundColor) { return; }
-        switch (this.colorType) {
-            case ColorType.GRAY_SCALE:
-            case ColorType.GRAY_SCALE_ALPHA:
-                return colorGrayScale(backgroundColor.gray);
-            case ColorType.PALETTE:
-                return colorPalette(backgroundColor.index);
-            case ColorType.RGB:
-            case ColorType.RGBA:
-                return colorRGB(backgroundColor.red, backgroundColor.green, backgroundColor.blue);
             default:
                 return undefined;
         }
@@ -264,28 +334,6 @@ export class PngImage {
     public rgbaAt(x: number, y: number): ColorRGBA {
         return convertToRGBA(this.at(x, y), this.palette);
     }
-
-    /**
-     * Retrieve the palette of this image if the color type is `ColorType.PALETTE`.
-     *
-     * @see ColorType
-     *
-     * @return The palette or `undefined` if a different color type was used.
-     */
-    public get palette(): Palette {
-        const palette = this.nativePng.palette;
-        if (!palette) { return; }
-        return palette.reduce((result: Palette, current: any, index: number) => {
-            result.set(index, colorRGB(current.red, current.green, current.blue));
-            return result;
-        }, new Map<number, ColorRGB>());
-    }
-
-    /**
-     * The gamma value of the image.
-     * Gathered from `png_get_gAMA`.
-     */
-    public get gamma(): number { return this.nativePng.gamma; }
 
     /**
      * Will encode this image to a PNG buffer.
