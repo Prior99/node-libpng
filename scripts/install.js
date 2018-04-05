d8538ee6678b3542420dca2fd09e46ec75b33840
Frederick Gnodtke <frederick@gnodtke.net>
2018-04-05 14:20:48 +0200

    Make node-libpng install behind a proxy

--- scripts/install.js
+++ scripts/install.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const fs = require("fs");
-const fetch = require("node-fetch");
+const request = require("request");
 const fileName = require("./file-name");
 
 if (fs.existsSync(fileName.qualifiedName)) { return; }
@@ -11,21 +11,26 @@ const pkg = require(path.resolve(__dirname, "..", "package.json"));
 const packageVersion = pkg.version;
 const url = `https://github.com/Prior99/node-libpng/releases/download/${packageVersion}/${fileName.baseName}`;
 
-fetch(url)
-    .then(response => {
-        if (response.ok) {
-            return response.buffer();
+console.info(`Downloading node-libpng prebuilt binary from "${url}".`);
+
+const destination = fs.createWriteStream(fileName.qualifiedName);
+
+request.get(url)
+    .on("error", err => { throw err; })
+    .on("response", response => {
+        if (response.statusCode >= 200 && response.statusCode < 300) {
+            response.pipe(destination);
+            destination.on("finish", () => {
+                console.info("Successfully downloaded binaries for node-libpng.");
+                destination.close();
+            });
+            return;
         }
-        if (response.status === 404) {
+        if (response.statusCode === 404) {
             throw new Error(`No supported node-libpng ${packageVersion} build found for node ${process.version} on ${process.platform} (${process.arch}).`);
         } else {
             throw new Error(`Error downloading binaries for node-libpng ${packageVersion}. Received status code ${response.statusCode}`)
         }
-    })
-    .catch(err => {
-        throw new Error(err);
-    })
-    .then(buffer => {
-        fs.writeFileSync(fileName.qualifiedName, buffer);
-        console.info("Successfully downloaded binaries for node-libpng.");
-    })
+        destination.close();
+        fs.unlink(fileName.qualifiedName);
+    });
