d8f36bb0223b1242f158cfaedb5e3e40b7e72dcf
Frederick Gnodtke <frederick@gnodtke.net>
2020-07-23 08:42:21 +0200

    Ignore file with wrong coverage report

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+/* istanbul ignore file */
 export { readPngFile, readPngFileSync, decode } from "./decode";
 export { writePngFile, writePngFileSync, encode } from "./encode";
 export { PngImage } from "./png-image";
